c0373632802378d3265bf17f14ac175cd3ef918f
aanchalmore2005 <aanchalmore1321@gmail.com>
2026-01-27 19:37:24 +0530

    Frontend updated

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -11,6 +11,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+
 // Cloudinary storage for Multer
 const cloudinaryStorage = new CloudinaryStorage({
   cloudinary: cloudinary,
